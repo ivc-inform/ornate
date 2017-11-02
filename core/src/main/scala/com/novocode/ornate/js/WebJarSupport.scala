488c957f3fcc284e9dde0faa0c17ba74421ddcad
andrew <uandrew1965@gmail.com>
2017-11-02 20:22:17 +0300

    .

--- core/src/main/scala/com/novocode/ornate/js/WebJarSupport.scala
+++ core/src/main/scala/com/novocode/ornate/js/WebJarSupport.scala
@@ -9,42 +9,55 @@ import org.webjars.WebJarAssetLocator
 
 import scala.collection.JavaConverters._
 import scala.io.Codec
+import java.io.InputStreamReader
 
 trait WebJarSupport {
-  import WebJarSupport._
 
-  def logger: Logger
+    import WebJarSupport._
 
-  def loadAsset(webjar: String, exactPath: String): Option[String] = getFullPathExact(webjar, exactPath).map { path =>
-    logger.debug(s"Loading WebJar asset: $webjar/$exactPath")
-    val in: InputStream = getClass.getClassLoader.getResourceAsStream(path)
+    def logger: Logger
 
-    try
-      in.content(Codec.UTF8).mkString
-    finally
-      in.close()
-  }
+    def loadAsset(webjar: String, exactPath: String): Option[String] = getFullPathExact(webjar, exactPath).map { path =>
+        logger.debug(s"Loading WebJar asset: $webjar/$exactPath")
+
+        val inputStream: InputStream = getClass.getClassLoader.getResourceAsStream(path)
+        val bufferSize = 1024
+        val buffer = new Array[Char](bufferSize)
+        val out = new StringBuilder
+        val in = new InputStreamReader(inputStream, "UTF-8")
+
+        try {
+            var rsz = in.read(buffer, 0, buffer.length)
+            while (rsz >= 0) {
+                out.append(buffer, 0, rsz)
+                rsz = in.read(buffer, 0, buffer.length)
+            }
+            out.toString()
+        }
+        finally
+            inputStream.close()
+    }
 }
 
 object WebJarSupport {
-  @volatile private lazy val fullPathIndex = {
-    val loader = Option(Thread.currentThread.getContextClassLoader).getOrElse(getClass.getClassLoader)
-    val fpi = WebJarAssetLocator.getFullPathIndex(Pattern.compile(".*"), loader)
-    val offset = WebJarAssetLocator.WEBJARS_PATH_PREFIX.length + 1
-    fpi.asScala.valuesIterator.collect { case s if s.startsWith(WebJarAssetLocator.WEBJARS_PATH_PREFIX) =>
-      val sep1 = s.indexOf('/', offset+1)
-      val sep2 = s.indexOf('/', sep1+1)
-      val noVer = s.substring(offset, sep1) + s.substring(sep2)
-      (noVer, s)
-    }.toMap
-  }
+    @volatile private lazy val fullPathIndex = {
+        val loader = Option(Thread.currentThread.getContextClassLoader).getOrElse(getClass.getClassLoader)
+        val fpi = WebJarAssetLocator.getFullPathIndex(Pattern.compile(".*"), loader)
+        val offset = WebJarAssetLocator.WEBJARS_PATH_PREFIX.length + 1
+        fpi.asScala.valuesIterator.collect { case s if s.startsWith(WebJarAssetLocator.WEBJARS_PATH_PREFIX) =>
+            val sep1 = s.indexOf('/', offset + 1)
+            val sep2 = s.indexOf('/', sep1 + 1)
+            val noVer = s.substring(offset, sep1) + s.substring(sep2)
+            (noVer, s)
+        }.toMap
+    }
 
-  /* This is orders of magnitude faster than WebJarLocator.getFullPathExact: */
-  def getFullPathExact(webjar: String, exactPath: String): Option[String] =
-    fullPathIndex.get(webjar + "/" + exactPath)
+    /* This is orders of magnitude faster than WebJarLocator.getFullPathExact: */
+    def getFullPathExact(webjar: String, exactPath: String): Option[String] =
+        fullPathIndex.get(webjar + "/" + exactPath)
 
-  def listAssets(webjar: String, path: String): Vector[String] = {
-    val prefix = if(path.startsWith("/")) webjar+path else webjar+"/"+path
-    fullPathIndex.keysIterator.filter(_.startsWith(prefix)).map(_.substring(prefix.length)).toVector
-  }
+    def listAssets(webjar: String, path: String): Vector[String] = {
+        val prefix = if (path.startsWith("/")) webjar + path else webjar + "/" + path
+        fullPathIndex.keysIterator.filter(_.startsWith(prefix)).map(_.substring(prefix.length)).toVector
+    }
 }
